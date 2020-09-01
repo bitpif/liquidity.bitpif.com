c0f5802490b4505723c56a3792cdb1a60407354b
BITPIF <50061328+bitpif@users.noreply.github.com>
2020-09-01 04:48:08 +0300

    first commit

---
+++ precache-manifest.c8861f4ccfb526da2f55adbcbca7c82c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b198956c4acf3f0fc3d6b35cab7dafa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea831b7c6f61b15a24b2",
+    "url": "/static/css/2.88b23fda.chunk.css"
+  },
+  {
+    "revision": "bb10c30bf9ad84a75d78",
+    "url": "/static/css/main.c848f08f.chunk.css"
+  },
+  {
+    "revision": "ea831b7c6f61b15a24b2",
+    "url": "/static/js/2.cf75d699.chunk.js"
+  },
+  {
+    "revision": "494df9e3171e2ba4ec112f721a9f1999",
+    "url": "/static/js/2.cf75d699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb10c30bf9ad84a75d78",
+    "url": "/static/js/main.4e8da3dc.chunk.js"
+  },
+  {
+    "revision": "6a0f4080c01b27c39107",
+    "url": "/static/js/runtime-main.fc417d15.js"
+  }
+]);
